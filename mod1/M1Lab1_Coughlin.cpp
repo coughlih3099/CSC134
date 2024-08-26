fbac5a5be0603f90e7fc7486bcd4ffc778a9ffaf
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2024-08-26 11:30:22 -0400

    Apples

---
+++ mod1/M1Lab1_Coughlin.cpp
@@ -0,0 +1,30 @@
+// CSC 134
+// M1Lab
+// Harley Coughlin
+// 08/26/2024
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int num_apples;
+    double price_per_apple;
+    double total_cost;
+
+    cout << "What is your name? \n";
+    cin >> name;
+    cout << "How many apples in stock? \n";
+    cin >> num_apples;
+    cout << "How much is one apple? \n";
+    cin >> price_per_apple;
+    cout << endl;
+
+    cout << "Welcome to the " << name << " orchard!" << endl;
+
+    total_cost = num_apples * price_per_apple;
+
+    cout << "You have " << num_apples << " apples" << endl;
+    cout << "Each apple costs $" << price_per_apple << endl;
+    cout << "The total is: $" << total_cost << endl;
+}
