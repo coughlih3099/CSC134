9329bc6e320c8eee9449a3a92932655f909b9abe
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2024-08-26 11:37:14 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+ CSC134
+ M2HW1 - Bronze
+ Harley Coughlin
+ 08/26/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double account_balance, amount_deposit, amount_withdrawal;
+    cout << "Question 1: \n";
+    cout << "Please enter the starting account balance: ";
+    cin >> account_balance;
+    cout << "Please enter amount of deposit: ";
+    cin >> amount_deposit;
+    cout << "Please enter amount of withdrawal: ";
+    cin >> amount_withdrawal;
+
+    return 0;
+}
