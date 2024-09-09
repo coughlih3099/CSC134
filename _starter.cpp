d2258e5b7c3c7c17d73ae427ad87e9d36e65a7a9
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2024-09-09 15:28:35 +0000

    works with formatting

---
+++ _starter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// CSC 134
+// M2Lab1 - Case Study
+// Harley Coughlin
+// 09/09/2024
+
+int main() {
+    // setup variable
+    string name;
+
+    // say hi
+    cout << "Hi, what's your name? ";
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
+
+    return 0;
+
+}
--- mod2/M2Lab1/M2Lab1_Coughlin.cpp
+++ mod2/M2Lab1/M2Lab1_Coughlin.cpp
@@ -1,39 +1,60 @@
-// CSC 134
-// M2Lab1
-// Harley Coughlin
-// 08/25/2024
-
-#include <ios>
 #include <iostream>
 #include <iomanip>
+using namespace std;
 
+// CSC 134
+// M2Lab1 - Case Study
+// Harley Coughlin
+// 09/09/2024
 
 int main() {
-    using std::cout, std::cin, std::endl;
+    // setup variable
+    string name;
+    int length;
+    int width;
+    int height;
+    int volume; // in cubic ft
 
+    // price relaped variables - in $
     const double COST_PER_CUBIC_FOOT = 0.23;
-    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    const double PRICE_PER_CUBIC_FOOT = 0.5;
+    double cost;
+    double customer_price;
+    double profit;
 
-    double length, width, height, volume;
-    double cost, charge, profit;
+    // say hi
+    /*
+    cout << "Hi, what's your name? ";
+    cin >> name;
+    cout << "Nice to meet you, " << name << "\n\n";
+    */
 
-    cout << std::setprecision(2) << std::fixed << std::showpoint;
-    cout << "Enter the dimensions of the crate (in feet):\n";
-    cout << "Length: ";
+    // ask user questions
+    cout << "Let's get the crate dimensions. (in feet)" << endl;
+    cout << "What's the length? ";
     cin >> length;
-    cout << "Width: ";
+    cout << "What's the width? ";
     cin >> width;
-    cout << "Height: ";
+    cout << "Finally, what's the height? ";
     cin >> height;
+    cout << "\n";
 
+    // TODO: calculations
     volume = length * width * height;
+    // cost calcs
     cost = volume * COST_PER_CUBIC_FOOT;
-    charge = volume * CHARGE_PER_CUBIC_FOOT;
-    profit = charge - cost;
+    customer_price = volume * PRICE_PER_CUBIC_FOOT;
+    profit = customer_price - cost;
 
-    cout << "The volume of the crate is " << volume << " cubic feet.\n";
-    cout << "Cost to build: $" << cost << endl;
-    cout << "Charge to customer: $" << charge << endl;
-    cout << "Profit: $" << profit << endl;
+    // output
+    cout << "Crate is: " << length << " x " << width << " x " << height << "\n";
+    cout << "Total volume is: " << volume << " cubic feet." << "\n\n";
+    // TODO: output cost, price, and profit
+    cout << setprecision(2) << fixed;
+    cout << "Cost for the crate is:" << setw(4) << "$" << cost << "\n";
+    cout << "Customer Price is:" << setw(8) << "$" << customer_price << "\n";
+    cout << "Profit for the crate is: $" << profit << endl;
+
     return 0;
+
 }
