5cfd5114d1a0002a77d45cfa30d55e7d83285b60
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-09-13 10:39:02 -0400

    I don't like this

---
+++ mod3/M3T/M3T3_Coughlin.cpp
@@ -0,0 +1,116 @@
+// CSC 134
+// M3T3 - RNG
+// Harley Coughlin
+// 09/13/2024
+
+#include <iostream>
+#include <limits>
+#include <random>
+
+using namespace std;
+
+
+void print_main_menu();
+int get_choice();
+void play_game();
+void print_game_menu();
+int roll_dice();
+
+
+int main() {
+    int num_rolls = 0;
+    int choice;
+    int dice_value;
+    bool keep_playing = true;
+    cout << "Let's play some Craps!\n";
+    while (keep_playing) {
+        print_main_menu();
+        choice = get_choice();
+        if (choice == 1) {
+            play_game();
+        } else if (choice == 2) {
+            keep_playing = false;
+        }
+    }
+
+    return 0;
+}
+
+
+void print_main_menu() {
+    cout << "+---------------+\n";
+    cout << "|   Main Menu   |\n";
+    cout << "+---------------+\n";
+    cout << "| 1. Start Game |\n";
+    cout << "| 2. Quit       |\n";
+    cout << "+---------------+\n";
+    cout << "\n";
+}
+
+
+int get_choice() {
+    int user_input;
+    cin >> user_input;
+    while (cin.fail() || user_input <= 0) {
+        if (cin.fail()) {
+            cin.clear();
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Please enter a valid integer greater than 0: ";
+    }
+    return user_input;
+}
+
+
+void play_game() {
+    bool done = false;
+    int choice;
+    int dice_value;
+    while (!done) {
+        print_game_menu();
+        choice = get_choice();
+        if (choice == 1) {
+            dice_value = roll_dice();
+            cout << "You rolled: " << dice_value << "\n";
+            switch (dice_value) {
+                case 2:
+                case 3:
+                case 12:
+                    cout << "Craps!\n";
+                    done = true;
+                    break;
+                case 7:
+                case 11:
+                    cout << "Win!\n";
+                    done = true;
+                    break;
+            }
+        } else if (choice == 2) {
+            done = true;
+        }
+    }
+}
+
+
+void print_game_menu() {
+    cout << "+---------------+\n";
+    cout << "|   Game Menu   |\n";
+    cout << "+---------------+\n";
+    cout << "| 1. Roll Dice  |\n";
+    cout << "| 2. Quit       |\n";
+    cout << "+---------------+\n";
+    cout << "\n";
+}
+
+
+int roll_dice() {
+    int die_1, die_2, total;
+    random_device r;
+    seed_seq seed(r(), r());
+    default_random_engine rando(seed);
+    uniform_int_distribution dice(1, 6);
+    die_1 = dice(rando);
+    die_2 = dice(rando);
+    total = die_1 + die_2;
+    return total;
+}
