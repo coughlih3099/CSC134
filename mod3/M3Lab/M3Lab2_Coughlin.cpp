2a27deb816c43553519ee9dd0710c2251c63808f
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-09-13 19:12:40 -0400

    get_grade function complete

--- mod3/M3Lab/M3Lab2_Coughlin.cpp
+++ mod3/M3Lab/M3Lab2_Coughlin.cpp
@@ -10,6 +10,24 @@
 using namespace std;
 
 
+int get_grade();
+
+
 int main() {
     return 0;
+}
+
+
+int get_grade() {
+    int user_input;
+    cin >> user_input;
+    while (cin.fail() || user_input < 0 || user_input > 100) {
+        if (cin.fail()) {
+            cin.clear();
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Please enter an integer between 0 and 100: ";
+        cin >> user_input;
+    }
+    return user_input;
 }
