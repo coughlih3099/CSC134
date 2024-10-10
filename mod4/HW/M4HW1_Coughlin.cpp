e5a2170e2ecd77fe19c59627a9c9a4b662c5f3ec
coughlih3099 <145471617+coughlih3099@users.noreply.github.com>
2024-10-10 23:36:46 +0000

    M4HW1: I accidentally did silver and gold at the same time.

--- mod4/HW/M4HW1_Coughlin.cpp
+++ mod4/HW/M4HW1_Coughlin.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M4HW1 - Times Tables - Bronze
+// M4HW1 - Times Tables - Gold
 // Harley Coughlin
 // 10/09/2024
 
@@ -10,25 +10,54 @@
  * 
  * Bronze:
  * Use a while loop to print the 5 times tables using the above format from 1 to 12.
+ * 
+ * Silver:
+ * Instead of doing 5, ask user for a number between 1 and 12
+ * 
+ * Gold:
+ * Add input validation.
  */
 
 
 #include <iostream>
+#include <limits>
+#include <cassert>
+#include <cstdint>
 
 
 using namespace std;
 
 
+int get_user_number();
+
+
 int main() {
-    const int NUMBER = 5;
     const int TIMES_MIN = 1;
     const int TIMES_MAX = 12;
+    cout << "Enter a number (1-12): ";
+    int user_number = get_user_number();
+    assert(user_number >= 1 && "User number is less than 1.");
+    assert(user_number <= 12 && "User number is greater than 12.");
 
     cout << "M4HW1 - Times Tables\n\n";
     for (int i = TIMES_MIN; i <= TIMES_MAX; i++) {
-        cout << NUMBER << " times " << i << " is " << NUMBER * i << "\n";
+        cout << user_number << " times " << i << " is " << user_number * i << "\n";
     }
 
 
     return 0;
+}
+
+int get_user_number() {
+    int user_number;
+    cin >> user_number;
+    while (cin.fail() || user_number < 1 || user_number > 12){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Please enter a number between 1 and 12: ";
+        cin >> user_number;
+    }
+    assert(user_number >= 1 && "User number is less than 1.");
+    assert(user_number <= 12 && "User number is greater than 12.");
+    return user_number;
 }
