199524e22d443c436a2b946590bed811f2a79176
Harley Coughlin <145471617+coughlih3099@users.noreply.github.com>
2024-12-09 23:18:22 -0500

    NCurses.Util.Utils: Updated string_to_chtype to include character attributes

--- ncurses/include/utils/util.hpp
+++ ncurses/include/utils/util.hpp
@@ -9,17 +9,20 @@
 #include <ncurses.h>
 #include <string>
 #include <vector>
+#include "../core/Window.hpp"
 
 
 /**
  * @brief Convert a string into a null-terminated array of chtype characters
  */
-inline std::vector<chtype> string_to_chtype(const std::string& string) {
+inline std::vector<chtype> string_to_chtype(const std::string& string, Window* window) {
     std::vector<chtype> ch_string;
     ch_string.reserve(string.size());
+    chtype attrs = window->get_attributes();
     for (char character : string) {
-        ch_string.push_back(static_cast<chtype>(character));
+        ch_string.push_back(static_cast<chtype>(character) | attrs);
     }
+
     ch_string.push_back(static_cast<chtype>('\0'));
     return ch_string;
 }
